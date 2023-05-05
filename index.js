254c6628ceffdba173b509fdee19ea3aff526d5e
Alasdair Greenland <60007754+alasdair-greenland@users.noreply.github.com>
2023-05-04 21:38:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 Array.from(document.body.children).forEach( ele => ele.style.display = "none" );
+
+alert($("#samplette-btn"));
+
+function htmlToElement(html) {
+  let template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+let html = "";
